d0be5d1c13567c2e6c8ccb190d2a86480777beb2
AlbertoMameli <mamelialberto2@gmail.com>
2025-06-20 17:04:53 +0200

    feat: consegna 1 e 2

--- la-mia-pizzeria/src/main/java/org/lessons/wdpt6/pizzeria/la_mia_pizzeria/controller/PizzaController.java
+++ la-mia-pizzeria/src/main/java/org/lessons/wdpt6/pizzeria/la_mia_pizzeria/controller/PizzaController.java
@@ -1,10 +1,12 @@
 package org.lessons.wdpt6.pizzeria.la_mia_pizzeria.controller;
 
+import org.lessons.wdpt6.pizzeria.la_mia_pizzeria.model.Pizza;
 import org.lessons.wdpt6.pizzeria.la_mia_pizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +17,14 @@ public class PizzaController {
 
     @GetMapping
     public String index(Model model) {
+        model.addAttribute("pizzas", pizzaRepository.findAll());//prendo la lista delle pizze chiamando la pizzarepository
         return "pizzas/index";
+    }
+
+      @GetMapping("/{id}")
+    public String show(@PathVariable(name = "id") Integer id, Model model){
+        Pizza pizza = pizzaRepository.findById(id).get();
+        model.addAttribute("pizza", pizza);
+        return "pizzas/show";
     }
 }
